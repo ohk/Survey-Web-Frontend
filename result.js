a11c586d332cba1bc1c5eb4461cf1ea7526a97b9
Ömer Hamid Kamışlı <hamidkamisli@icloud.com>
2020-05-05 01:57:03 +0300

    Bug Fixes

--- result.js
+++ result.js
@@ -85,3 +85,20 @@ $(function () {
         })
     })
 })
+
+function createPDF() {
+    var doc = new jsPDF()
+    var elementHTML = $('.results-container').html()
+    /*var specialElementHandlers = {
+        '#elementH': function (element, renderer) {
+            return true
+        }
+    }*/
+    doc.fromHTML(elementHTML, 15, 15, {
+        width: 170
+        //,elementHandlers: specialElementHandlers
+    })
+
+    // Save the PDF
+    doc.save('sample-document.pdf')
+}
